436b0071b0bd11a028da8e3da44d86b1de5fc6fc
deepak76901 <deepakb76901@gmail.com>
2023-02-16 19:30:06 +0530

    Second Commit

---
+++ ConditionlStatement.java
@@ -0,0 +1,134 @@
+class ConditionalStatement {
+    public static void main(String []args){
+        
+
+        // TERNARY OPERATOR
+
+        // Example
+        // int n1=5,n2=10,max;
+        // System.out.println("First num: " + n1);
+        // System.out.println("Second num: " + n2);
+
+        // max = (n1>n2)? n1 : n2 ;
+        
+        // System.out.println("Maximum is = " + max);
+
+        // Trying
+        // Bhut der Baad Samajh aaya h Yaad rakhiyo
+
+        // int res = 20;
+        // String str = (res%2==1) ? "Odd" : "Even" ;
+        // System.out.println(str);
+                
+        // IF-ELSE STATEMENTS
+
+        // int val=8;
+        // if (val>5 && val<10){
+        //     System.out.println(val);
+        // }
+
+        // int value=13;
+        // if (value<5 || value>10){
+        //     System.out.println(value);
+        // }
+
+        // int mod =15;
+        // if (mod%2==0){
+        //     System.out.println("Value Entered is Even");
+        // }
+        // else{
+        //     System.out.println("Value Entered is Odd");
+        // }
+
+        // Q.  Find if the input character is ‘a’ or not.
+
+        // String alp = "a";
+        
+        // if (alp == "a"){
+        //     System.out.println("Entered alphabet is equal to a");
+        // }
+        // else{
+        //     System.out.println(("Entered alphabet is not equal to a"));
+        // }
+
+        // Q. Write a program to identify people as “Child” (age < 12), “Teenager” (12 <= age < 18) or “Adult” (age >= 18).
+
+        // int age = 18;
+        // if (age<=12){
+        //     System.out.println("You are a Child");
+        // }
+        // else if (age>12 && age<18){
+        //     System.out.println("You are a Teenager");
+        // }
+        // else if (age>=18){
+        //     System.out.println("You are an Adult");
+        // }
+        // else{
+        //     System.out.println("Invalid age");
+        // }
+
+        // Q.2. Print the maximum of 3 numbers a, b, c taken as input.
+
+        // int a=100;
+        // int b=50;
+        // int c=300;
+        // String max = "Maximum Numberr is ";
+        // if(a>b && b>c){
+        //     System.out.println(max + a);
+        // }
+        // else if (b>a && a>c){
+        //     System.out.println(max + b);
+        // }
+        // else{
+        //     System.out.println(max + c);
+        // }
+        
+
+        // SWITCH CASE STATEMMENTS.
+        
+        // EXAMPLE. Write a program using switch statements to check if the input lowercase character is vowel or consonant.
+        
+        // String ch = "e";
+        // switch (ch){
+        //     case "a" : System.out.println("vowel");
+        //     break;
+        //     case "e" : System.out.println("vowel");
+        //     break;
+        //     case "i" : System.out.println("vowel");
+        //     break;
+        //     case "o" : System.out.println("vowel");
+        //     break;
+        //     case "u" : System.out.println("vowel");
+        //     break;
+        //     default: System.out.println("Consonant");
+            
+        // }
+
+
+        // Q. Write a program to print the day name based upon the day number.
+
+        // String day = "11";
+        // switch (day){
+        //     case "1" : System.out.println("Day 1 is Monday");
+        //     break;
+        //     case "2" : System.out.println("Day 2 is Tuesday");
+        //     break;
+        //     case "3" : System.out.println("Day 3 is Wednesday");
+        //     break;
+        //     case "4" : System.out.println("Day 4 is Thursday");
+        //     break;
+        //     case "5" : System.out.println("Day 5 is Friday");
+        //     break;
+        //     case "6" : System.out.println("Day 6 is Saturday");
+        //     break;
+        //     case "7" : System.out.println("Day 7 is Sunday");
+        //     break;
+        //     default: System.out.println("You Entered a Wrong Number");
+        // }
+
+
+
+        
+
+}
+}
