fe295a66bf1fa631e49cf14613529889034fae49
deepak76901 <deepakb76901@gmail.com>
2023-02-17 20:25:31 +0530

    Scanners Topic

---
+++ ScannerClass.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ScannerClass {
+
+    public static void main(String[] args) {
+        // Scanner scan = new Scanner(System.in);
+        // System.out.println("Enter First Number : ");
+        // int a = scan.nextInt();
+        // System.out.println("Enter Second Number :");
+        // int b = scan.nextInt();
+
+        // System.out.println("The Sum of 2 numbers is " + (a+b));
+
+        // Scanner scan = new Scanner(System.in);
+        // System.out.println("Enter the Username : ");
+        // String username = scan.next();
+        // System.out.println("Enter the password : ");
+        // String password = scan.next();
+        // System.out.println("The Username is " + username);
+        // System.out.println("The password is " + password);
+
+    }
+    
+}
