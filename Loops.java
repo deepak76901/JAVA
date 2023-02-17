6bae2fa125ad317704746aba87130e09cdfbc9fd
deepak76901 <deepakb76901@gmail.com>
2023-02-17 20:03:28 +0530

    Loops Commit kr rha hu 17feb

---
+++ Loops.java
@@ -0,0 +1,94 @@
+class Loops {
+    public static void main(String[] args) {
+
+        // WHILE LOOP
+
+        // Q.1 Print the sum of the first ‘n’ natural numbers using a While loop, where
+        // n is the input
+
+        // int n=4,sum=0,i=1;     
+        
+        // while (i<=n){
+        //     System.out.println(i);
+        //     sum = sum +i;
+        //     i++;
+        // }
+        // System.out.println("Sum of First n Natural is " + sum);
+
+
+        // FOR LOOP
+
+        // Q  Print the sum of the first 10 natural numbers using a for loop
+
+        // int n =3,sum=0,j=1;
+        // for (; j<=n;j++){
+        //     System.out.println(j);
+        //     sum=sum+j;            
+        // }
+        // System.out.println(sum);     
+
+        // DO WHILE LOOP
+        
+        // Q. Print the sum of the first 10 natural numbers using do while loop.
+        
+        // int n=10,sum =0,k=1;
+        // do{
+        //     System.out.print(k + " ");
+        //     sum = sum+k;
+        //     k++;
+        // }while(k<=n);
+        // System.out.print(sum);
+
+        // NESTED LOOPS
+
+        // int n=5;
+        
+        // for(int j=1;j<=n;j++){
+
+        // for (int i=1;i<=n;i++){
+        //     System.out.print(i + " ");
+        //     if (i==j) break;
+        // }
+        // System.out.println();
+        // }
+
+
+        // BREAK AND CONTINUE 
+
+        // Q Print the first multiple of 5 which is also a multiple of 7
+
+        // int a=300,n = 5, m=7;
+        // for(int i=1; i<=a; i++){
+        //     if(i%n==0 && i%m==0){
+        //         // if(i%2==0 && i%5==0 && i%7==0) continue;
+        //         System.out.println(i);
+        //         // break;
+        //     }
+        // }
+
+        // Q Tell if the number in the input is prime or not.
+
+        // int n=17;
+        // int temp=0;
+        // for(int i=2; i<=n-1 ; i++){
+        //     if (n%i==0){
+        //         temp = temp +1;
+                
+        //     }
+        // }
+        // if(temp==0){
+        //     System.out.println("Number is  prime");
+
+        // }
+        // else {
+        //     System.out.println("Number is not a Priime");
+        // }
+
+        
+        
+
+        
+        
+
+    }
+}
