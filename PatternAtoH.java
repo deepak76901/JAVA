a6bfa1ccb7c9f090ef19d8d1399cdd13b73c39f2
deepak76901 <deepakb76901@gmail.com>
2023-02-18 21:20:03 +0530

    Pattern of Alphabet

---
+++ PatternAtoH.java
@@ -0,0 +1,138 @@
+class PatternAtoH {
+
+    public static void main(String[] args) {
+
+        // PATTERN OF A
+
+        // int n = 11;
+        // for (int i = 0; i < n; i++) {
+        //     for (int j = 0; j < n; j++) {
+        //         if (j == 0 && i>0 || j == (n - 1) && i>0 || i==0 && j>0 && j<(n-1) || i == (n - 1) / 2  ) {
+        //             System.out.print("*");
+
+        //         } else {
+        //             System.out.print(" ");
+
+        //         }
+        //     }
+        //     System.out.println();
+        // }
+
+        // PATTERN OF B
+
+        // int n = 11;
+        // for (int i = 0; i < n; i++) {
+        //     for (int j = 0; j < n; j++) {
+        //         if (j == 0 || i==(n-1) && j<(n-1) || j == (n - 1) && i>0 && i<(n-1)/2   || i==0 && j>0 && j<(n-1) || i == (n-1)/2 && j<(n-1) || j == (n - 1) && i<(n-1) && i>(n-1)/2   ) {
+        //             System.out.print("*");
+
+        //         } else {
+        //             System.out.print(" ");
+
+        //         }
+        //     }
+        //     System.out.println();
+        // }
+        
+
+        // PATTERN OF C
+
+        // int n = 11;
+        // for (int i = 0; i < n; i++) {
+        //     for (int j = 0; j < n; j++) {
+        //         if (i==0 || i==(n-1) || j==0) {
+        //             System.out.print("*");
+
+        //         } else {
+        //             System.out.print(" ");
+
+        //         }
+        //     }
+        //     System.out.println();
+        // }
+
+        // PATTERN OF D
+
+        // int n = 11;
+        // for (int i = 0; i < n; i++) {
+        //     for (int j = 0; j < n; j++) {
+        //         if (i==0 && j<(n-2) || i==(n-1) && j<(n-2) || j==0 || j==(n-1) && i>0 && i<(n-1) ) {
+        //             System.out.print("*");
+
+        //         } else {
+        //             System.out.print(" ");
+
+        //         }
+        //     }
+        //     System.out.println();
+        // }
+
+        // PATTERN OF E
+
+        // int n = 11;
+        // for (int i = 0; i < n; i++) {
+        //     for (int j = 0; j < n; j++) {
+        //         if (i==0 ||i==(n-1)/2 || i==(n-1) || j==0 ) {
+        //             System.out.print("*");
+
+        //         } else {
+        //             System.out.print(" ");
+
+        //         }
+        //     }
+        //     System.out.println();
+        // }  
+
+        // PATTERN OF F
+
+        // int n = 11;
+        // for (int i = 0; i < n; i++) {
+        //     for (int j = 0; j < n; j++) {
+        //         if (i==0 ||i==(n-1)/2  || j==0 ) {
+        //             System.out.print("*");
+
+        //         } else {
+        //             System.out.print(" ");
+
+        //         }
+        //     }
+        //     System.out.println();
+        // }  
+
+        // PATTERN OF G
+
+        // int n = 11;
+        // for (int i = 0; i < n; i++) {
+        //     for (int j = 0; j < n; j++) {
+        //         if (i==0 || j==0 || i==(n-1) && j<=(n-1)/2 || j==(n-1)/2 && i>=(n-1)/2 || i==(n-1)/2 && j>=(n-1)/2 || j==(n-1) && i>=(n-1)/2 ) {
+        //             System.out.print("*");
+
+        //         } else {
+        //             System.out.print(" ");
+
+        //         }
+        //     }
+        //     System.out.println();
+        // }  
+
+        // PATTERN OF H
+        // int n = 11;
+        // for (int i = 0; i < n; i++) {
+        //     for (int j = 0; j < n; j++) {
+        //         if (j==0 || j==(n-1) || i==(n-1)/2) {
+        //             System.out.print("*");
+
+        //         } else {
+        //             System.out.print(" ");
+
+        //         }
+        //     }
+        //     System.out.println();
+        // }  
+        
+
+
+
+    }
+
+}
