f6205ccf945ac89663e5091e28fc25dee0442644
deepak76901 <deepakb76901@gmail.com>
2023-02-20 19:36:19 +0530

    Arrays anonymous ni hua

---
+++ Arrays.java
@@ -0,0 +1,68 @@
+public class Arrays {
+    public static void main(String[] args) {
+        // int obj[] = new int[6];
+        // obj[0]=50;
+        // obj[1]=40;
+        // obj[2]=70;
+        // obj[3]=40;
+        // obj[4]=50;
+        // obj[5]=20;
+
+        // for(int i:obj){
+        //     System.out.println(i);
+        // }
+
+        // for(int i=0;i<obj.length;i++){
+        // System.out.println(obj[i]);
+        // }
+
+        // int obj1[]={1,2,3,4,5};
+        // for(int i=0;i<obj1.length;i++)
+        // {
+        // System.out.println(obj1[i]);
+        // }
+
+        // int obj2[][] = new int[3][2];
+        // obj2[0][0] = 12;
+        // obj2[0][1] = 9;
+        // obj2[1][0] = 4;
+        // obj2[1][1] = 32;
+        // obj2[2][0] = 6;
+        // obj2[2][1] = 8;
+        // for (int i = 0; i < 3; i++) {
+        // for (int j = 0; j < 2; j++) {
+        // System.out.print(obj2[i][j]+ " ");
+        // // System.out.print(" ");
+
+        // }
+        // System.out.println();
+        // }
+        // for (int a : obj2){
+        //     System.out.println(a);
+        // }
+
+        int obj3[][] = {
+                { 2, 3 ,8},
+                { 6, 2 },
+                { 7, 9 ,1 ,6 }
+        };
+        // for(int i=0;i<obj3.length;i++){
+        //     for(int j=0;j<obj3.length;j++){
+        //         System.out.println(obj3[i][j]);
+
+        //     }
+        //     System.out.println();
+        // }
+
+        // for(int a[] : obj3){
+        //     for(int b : a){
+        //         System.out.print(b + " ");
+        //     }
+        //     System.out.println();
+        // }
+
+
+        System.out.println(obj3[1].length);
+
+    }
+}
